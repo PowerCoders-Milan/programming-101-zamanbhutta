5b43bc54ffc01424c20db2a6ca306fe7205b1bca
zamanbhutta <marham.ayat@gmail.com>
2021-02-18 13:33:26 +0100

    exercise 2

--- 02-Sorting-Numbers/sorting_numbers.js
+++ 02-Sorting-Numbers/sorting_numbers.js
@@ -4,3 +4,21 @@ here
 */
 
 //insert your code below
+var num;
+
+function listsGetSortCompare(type, direction) {
+  var compareFuncs = {
+    "NUMERIC": function(a, b) {
+        return Number(a) - Number(b); },
+    "TEXT": function(a, b) {
+        return a.toString() > b.toString() ? 1 : -1; },
+    "IGNORE_CASE": function(a, b) {
+        return a.toString().toLowerCase() > b.toString().toLowerCase() ? 1 : -1; },
+  };
+  var compare = compareFuncs[type];
+  return function(a, b) { return compare(a, b) * direction; }
+}
+
+
+num = [1, 4, 3];
+window.alert(num.slice().sort(listsGetSortCompare("NUMERIC", 1)));
